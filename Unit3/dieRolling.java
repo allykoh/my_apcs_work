755eb564fbf1e88f29a089fc68b211cb6e54e87e
allykoh <emailallyk@gmail.com>
2025-10-31 00:42:52 -0700

    Add files via upload

---
+++ Unit3/dieRolling.java
@@ -0,0 +1,14 @@
+import java.lang.Math; 
+public class dieRolling {
+	public static int dieRolling(){
+		int dice = (int)(Math.random() * 6 + 1); 
+			System.out.println(dice); 
+	}
+	
+	
+	public static void main (String[] args) {
+		
+	}
+}
+}
+
