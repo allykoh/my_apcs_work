2f669a980083a4e8cce91022a15021c9df59087f
allykoh <emailallyk@gmail.com>
2025-11-01 15:15:19 -0700

    unit3

--- Unit3/dieRolling.java
+++ Unit3/dieRolling.java
@@ -1,13 +1,15 @@
-import java.lang.Math; 
+
 public class dieRolling {
-	public static int dieRolling(){
-		int dice = (int)(Math.random() * 6 + 1); 
-			System.out.println(dice); 
+	public static int rollDie(){
+		int dice = (int)(Math.random() * 6) + 1; 
+			return dice; 
 	}
 	
-	
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+		System.out.println("Rolling 5 times...");
+		for(int i = 1; i <= 5; i++){
+			int result = rollDie(); 
+			System.out.println("Roll " + i + " : " + result); 
 	}
 }
 }
