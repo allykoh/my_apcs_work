6ff89dc4c4e8fcfe8518ce05d82193aa3f230ed6
allykoh <emailallyk@gmail.com>
2025-12-03 14:35:50 -0800

    unit 7

--- Unit7/countLucky.java
+++ Unit7/countLucky.java
@@ -1,20 +1,31 @@
-public class countLucky {
+
+public class CountLucky{
 	
-	public static int countLucky(int[] arr){
-	int count = 0; 
-	for(int num : arr){
-		if(num%7 == 0 || num%10 == 7){
-			count++
+	public static int countLucky(int[] n){
+		int value = 0; 
+		for(int i = 0; i<n.length; i++){
+			if(n[i] == 7 || n[i]%10 == 7){
+				value++; 
+			}
+		}
+		return value; 
+	}
+	
+	public static void testLucky(int[] n, int expected){
+		int actual = countLucky(n); 
+		
+		System.out.println("Numbers: " + java.util.Arrays.toString(n) + " # of lucky numbers: " + actual + " we got: " + expected); 
+		if(actual == expected){
+			System.out.println("Nice!"); 
+		}else{
+			System.out.println("Oof");
 		}
 	}
-	return count; 
-}
-	public static void testcountLucky(){
-		
-	}
+	
 	public static void main (String[] args) {
-		
-		
+		testLucky(new int[]{0, 7, 8, 17}, 2); 
+		testLucky(new int[]{1, 2, 3, 4, 5}, 0);
+		testLucky(new int[]{19, 17, 20}, 1); 
 	}
-
+}
 
